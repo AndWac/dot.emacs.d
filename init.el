213d7ee244415c5ea10fef24e8af7c464bdac08c
Andreas Wacknitz <a.wacknitz@gmx.de>
2019-09-19 18:35:53 +0200

    Move my init.el into an org file

--- init.el
+++ init.el
@@ -1,1 +1,19 @@
-init-single-file-version.el
+;; Boostrapping
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("org" . "https://orgmode.org/elpa/"))
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/") t)
+(setq package-archive-priorities '(("org" . 3)
+                                   ("melpa" . 2)
+                                   ("gnu" . 1)))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(org-babel-load-file (expand-file-name "myinit.org" user-emacs-directory))
