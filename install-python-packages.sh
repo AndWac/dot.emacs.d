2c6d2e3bb1d8899dd9b06523a7890a6677a9f616
Andreas Wacknitz <A.Wacknitz@gmx.de>
2020-06-20 18:26:21 +0200

    add install-python-packages.sh

---
+++ install-python-packages.sh
@@ -0,0 +1,13 @@
+# Install some interesting python packages, eg. supported by emacs eglot:
+sudo pip install python-language-server
+sudo pip install pycodestyle pylint mccabe pydocstyle
+# rope is used by eglot
+sudo pip install rope
+# jedi can be used by elpy (instead of rope):
+#pip install jedi
+# flake8 for code checks
+sudo pip install flake8
+# and autopep8 for automatic PEP8 formatting
+sudo pip install autopep8
+# and yapf for code formatting
+sudo pip install yapf
